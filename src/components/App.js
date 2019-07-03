933ca0ccdf16de452cd37bbe0f85495ce2ffcddc
warda <warda@care3.co>
2019-07-03 17:44:45 +0000

    initial app setup

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+
+function App() {
+  return (
+    <div className="App">
+      <p>App</p>
+    </div>
+  );
+}
+
+export default App;
