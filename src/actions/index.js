6368dd74b84a2967ea04e2ce6669e5d1382aa98e
warda <warda@care3.co>
2019-07-08 21:58:30 +0000

    attach userId to new streams being created

--- src/actions/index.js
+++ src/actions/index.js
@@ -22,8 +22,9 @@ export const signOut = () => {
   };
 };
 
-export const createStream = (formValues) => async dispatch => {
-  const response = await streams.post('/streams', formValues);
+export const createStream = (formValues) => async (dispatch, getState) => {
+  const { userId } = getState().auth;
+  const response = await streams.post('/streams', { ...formValues, userId });
 
   dispatch({ type: CREATE_STREAM, payload: response.data });
 };
@@ -46,7 +47,7 @@ export const editStream = (id, formValues) => async dispatch => {
   dispatch({ type: EDIT_STREAM, payload: response.data })
 };
 
-export const fetchStreams = (id) => async dispatch => {
+export const deleteStream = (id) => async dispatch => {
   await streams.delete(`/streams/${id}`);
 
   dispatch({ type: DELETE_STREAM, payload: id })
