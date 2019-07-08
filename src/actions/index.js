e89770ba376871531f01f957d5678606a9c56e89
warda <warda@care3.co>
2019-07-08 19:35:17 +0000

    action creator for creating a stream

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,5 @@
+import streams from '../apis/streams';
+
 export const signIn = (userId) => {
   return {
     type: 'SIGN_IN',
@@ -10,3 +12,7 @@ export const signOut = () => {
     type: 'SIGN_OUT'
   };
 };
+
+export const createStream = (formValues) => async dispatch => {
+  streams.post('/streams', formValues);
+}
