8ca5790f531c31a15ec6f2a4e0f65a3377abed6a
warda <warda@care3.co>
2019-07-09 19:54:44 +0000

    nested a modal component

--- src/components/streams/StreamDelete.js
+++ src/components/streams/StreamDelete.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import Modal from '../Modal';
 
 const StreamDelete = () => {
   return (
     <div>
       StreamDelete
-      </div>
+      <Modal />
+    </div>
   );
 };
 
