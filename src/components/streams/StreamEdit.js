04cdce9eee1446b8ab3e9af5607be22bd9323520
warda <warda@care3.co>
2019-07-09 17:41:19 +0000

    mapped stream id to state from url/route params

--- src/components/streams/StreamEdit.js
+++ src/components/streams/StreamEdit.js
@@ -1,6 +1,7 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
-const StreamEdit = () => {
+const StreamEdit = (props) => {
   return (
     <div>
       StreamEdit
@@ -8,4 +9,8 @@ const StreamEdit = () => {
   );
 };
 
-export default StreamEdit;
+const mapStateToProps = (state, ownProps) => {
+  return { stream: state.streams[ownProps.match.params.id] }
+}
+
+export default connect(mapStateToProps)(StreamEdit);
