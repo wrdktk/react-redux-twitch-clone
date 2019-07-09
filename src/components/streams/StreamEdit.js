09b331e9a3f88e1a511fa748c6c23f025702dc1e
warda <warda@care3.co>
2019-07-09 18:24:34 +0000

    added action creator to prefetch streams

--- src/components/streams/StreamEdit.js
+++ src/components/streams/StreamEdit.js
@@ -1,16 +1,23 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { fetchStream } from '../../actions';
+class StreamEdit extends React.Component {
 
-const StreamEdit = (props) => {
-  return (
-    <div>
-      StreamEdit
-      </div>
-  );
-};
+  componentDidMount() {
+    const streamId = this.props.match.params.id;
+    this.props.fetchStream(streamId);
+  }
+
+  render() {
+    if (!this.props.stream) {
+      return <div>Loading...</div>
+    }
+    return <div>{this.props.stream.title}</div>
+  }
+}
 
 const mapStateToProps = (state, ownProps) => {
   return { stream: state.streams[ownProps.match.params.id] }
 }
 
-export default connect(mapStateToProps)(StreamEdit);
+export default connect(mapStateToProps, { fetchStream })(StreamEdit);
