0466da2eb86ade1e358470576e8a4c0c9ba7e7bb
warda <warda@care3.co>
2019-07-09 22:41:05 +0000

    added semicolons to dispatch + programatic navigation

--- src/actions/index.js
+++ src/actions/index.js
@@ -35,23 +35,26 @@ export const createStream = (formValues) => async (dispatch, getState) => {
 export const fetchStreams = () => async dispatch => {
   const response = await streams.get('/streams');
 
-  dispatch({ type: FETCH_STREAMS, payload: response.data })
+  dispatch({ type: FETCH_STREAMS, payload: response.data });
 };
 
 export const fetchStream = (id) => async dispatch => {
   const response = await streams.get(`/streams/${id}`);
 
-  dispatch({ type: FETCH_STREAM, payload: response.data })
+  dispatch({ type: FETCH_STREAM, payload: response.data });
 };
 
 export const editStream = (id, formValues) => async dispatch => {
   const response = await streams.patch(`/streams/${id}`, formValues);
 
-  dispatch({ type: EDIT_STREAM, payload: response.data })
+  dispatch({ type: EDIT_STREAM, payload: response.data });
+  history.push('/');
+
 };
 
 export const deleteStream = (id) => async dispatch => {
   await streams.delete(`/streams/${id}`);
 
-  dispatch({ type: DELETE_STREAM, payload: id })
+  dispatch({ type: DELETE_STREAM, payload: id });
+  history.push('/');
 };
